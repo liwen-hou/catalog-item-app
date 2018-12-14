e7e5a7a6ea060b5feb58bdc5dfe28281e128440d
Hou Liwen <lhou@vmware.com>
2018-12-14 15:44:02 +0800

    added authentication for database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -54,6 +54,6 @@ class CatalogItem(Base):
                 }
 
 
-engine = create_engine('postgresql:///catalogitemswithusers.db')
+engine = create_engine('postgresql://catalog:password/catalogitemswithusers.db')
 
 Base.metadata.create_all(engine)
